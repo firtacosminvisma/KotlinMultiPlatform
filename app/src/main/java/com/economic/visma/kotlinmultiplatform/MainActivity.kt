e567501211b86e3b28da237fab28672ee95edc69
cosmin <cosmin.firta@visma.com>
2019-05-03 11:45:39 +0300

    android implementation

--- app/src/main/java/com/economic/visma/kotlinmultiplatform/MainActivity.kt
+++ app/src/main/java/com/economic/visma/kotlinmultiplatform/MainActivity.kt
@@ -2,15 +2,17 @@ package com.economic.visma.kotlinmultiplatform
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import com.example.mobileconf.shared.HelloWorld
+import com.example.mobileconf.shared.GitHubApi
+import kotlinx.android.synthetic.main.activity_main.hello_world_text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val helloWorld = HelloWorld()
-        Log.d("SharedCode", helloWorld.greetings())
+        val githubApi = GitHubApi()
+        hello_world_text.text = "loading ..."
+        githubApi.load {
+            hello_world_text.text = "result: ${it.url}"
+        }
     }
 }
