9921aeb91f6c246d79b6d983ac2e459462ecafdb
cosmin <cosmin.firta@visma.com>
2019-05-04 10:17:01 +0300

    Expect/Actual Greeting Implementation on Android

--- app/src/main/java/com/economic/visma/kotlinmultiplatform/MainActivity.kt
+++ app/src/main/java/com/economic/visma/kotlinmultiplatform/MainActivity.kt
@@ -2,15 +2,14 @@ package com.economic.visma.kotlinmultiplatform
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import com.example.mobileconf.shared.HelloWorld
+import com.example.mobileconf.shared.Greeting
+import kotlinx.android.synthetic.main.activity_main.hello_world_text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val helloWorld = HelloWorld()
-        Log.d("SharedCode", helloWorld.greetings())
+        val greeting = Greeting()
+        hello_world_text.text = greeting.greetMe()
     }
 }
